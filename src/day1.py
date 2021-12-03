e9d5ea970bdc8bd9fa5bddd2b30d4ff5eb204ec0
marvelman3284 <jedimaster3284@gmail.com>
2021-12-03 13:21:17 -0500

    Formatting for days 1 and 2

--- src/day1.py
+++ src/day1.py
@@ -2,24 +2,25 @@ def count_increases(arr: list) -> int:
     inc = 0
     for i in range(len(arr)):
         try:
-            if arr[i+1] > arr[i]:
+            if arr[i + 1] > arr[i]:
                 inc += 1
         except IndexError:
-            if arr[i] > arr[i-1]:
+            if arr[i] > arr[i - 1]:
                 inc += 1
             break
-    
+
     return inc
+
 
 def count_3_increases(arr: list[int]) -> int:
     inc3 = 0
     for i in range(len(arr)):
         try:
-            sum1 = arr[i] + arr[i+1] + arr[i+2]
-            sum2 = arr[i+1] + arr[i+2] + arr[i+3]
+            sum1 = arr[i] + arr[i + 1] + arr[i + 2]
+            sum2 = arr[i + 1] + arr[i + 2] + arr[i + 3]
 
-            print(f"First({i}): ", arr[i] , arr[i+1] , arr[i+2])
-            print("2nd: ", arr[i+1] , arr[i+2] , arr[i+3])
+            print(f"First({i}): ", arr[i], arr[i + 1], arr[i + 2])
+            print("2nd: ", arr[i + 1], arr[i + 2], arr[i + 3])
             if sum2 > sum1:
                 inc3 += 1
         except IndexError:
@@ -32,13 +33,13 @@ def main():
     inputs = []
     with open("data/day1.txt", "r") as f:
         for i in f.readlines():
-            i.replace('\n', '')
+            i.replace("\n", "")
             inputs.append(int(i))
-
 
     print(count_increases(inputs))
 
     print(count_3_increases(inputs))
+
 
 if __name__ == "__main__":
     main()
--- src/day2.py
+++ src/day2.py
@@ -1,10 +1,11 @@
 def split_arr(arr: list) -> list[str]:
     for i, j in enumerate(arr):
-        arr[i] = j.split(" ") 
+        arr[i] = j.split(" ")
 
     return arr
 
-def get_pos_w_aim(arr:list) -> int:
+
+def get_pos_w_aim(arr: list) -> int:
     horz = 0
     depth = 0
     aim = 0
@@ -31,17 +32,19 @@ def get_pos(arr: list) -> int:
         elif i[0] == "up":
             vert -= int(i[1])
 
-    return horz*vert
+    return horz * vert
+
 
 def main():
     inputs = []
 
-    with open('data/day2.txt', 'r') as f:
+    with open("data/day2.txt", "r") as f:
         for i in f.readlines():
             i = i.replace("\n", "")
             inputs.append(i)
 
     print(get_pos_w_aim(split_arr(inputs)))
+
 
 if __name__ == "__main__":
     main()
